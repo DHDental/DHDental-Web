63c8bb44bba772422241cd9f445d9928f6a64c8c
khanhhoa20 <khanhhoa12t2@gmail.com>
2022-09-23 17:00:10 +0700

    yup validate login

--- src/pages/login/LoginForm.jsx
+++ src/pages/login/LoginForm.jsx
@@ -1,6 +1,29 @@
 import { Box, Button, Card, CardContent, CardHeader, Grid, TextField } from '@mui/material'
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
+const validationSchema = yup.object({
+    phone: yup
+        .string()
+        .required('Bắt buộc nhập số điện thoại')
+        .matches(/^[0-9]+$/, "Bạn cần nhập số")
+        .min(10, 'Bạn cần nhập số điện thoại có 10 số')
+        .max(10, 'Bạn cần nhập số điện thoại có 10 số'),
+    password: yup
+        .string()
+        .required('Bắt buộc nhập mật khẩu'),
+});
 const LoginForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            phone: '',
+            password: '',
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
     return (
         <>
             <Grid container justifyContent='center'>
@@ -21,20 +44,35 @@ const LoginForm = () => {
                             sx={{
                                 display: 'flex',
                             }}
+                            onSubmit={formik.handleSubmit}
                         >
                             <Grid container direction="column" spacing={2}>
                                 <Grid item>
                                     <TextField
+                                        id='phone'
+                                        name='phone'
                                         fullWidth
                                         variant="outlined"
-                                        label="Số điện thoại" />
+                                        label="Số điện thoại"
+                                        value={formik.values.phone}
+                                        onChange={formik.handleChange}
+                                        error={formik.touched.phone && Boolean(formik.errors.phone)}
+                                        helperText={formik.touched.phone && formik.errors.phone}
+                                    />
+
                                 </Grid>
                                 <Grid item>
                                     <TextField
+                                        id='password'
+                                        name='password'
                                         fullWidth
                                         variant="outlined"
                                         label="Mật khẩu"
                                         type="password"
+                                        value={formik.values.password}
+                                        onChange={formik.handleChange}
+                                        error={formik.touched.password && Boolean(formik.errors.password)}
+                                        helperText={formik.touched.password && formik.errors.password}
                                     />
                                 </Grid>
                                 <br />
@@ -42,6 +80,7 @@ const LoginForm = () => {
                                     <Button
                                         variant="contained"
                                         fullWidth
+                                        type='submit'
                                     >
                                         Đăng nhập
                                     </Button>
