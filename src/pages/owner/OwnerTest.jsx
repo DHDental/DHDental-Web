ec9ea76f8879fa3b9f5bf6d2350fa6372918feb4
LNTanTai <Tailainguyentan@gmail.com>
2022-11-27 21:56:52 +0700

    update Owner and Admin Page

--- src/pages/owner/OwnerTest.jsx
+++ src/pages/owner/OwnerTest.jsx
@@ -1,81 +1,214 @@
 import React, { useEffect, useState } from "react";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
-import {
-  Box,
-} from "@mui/material";
+import { Alert, Box, Snackbar } from "@mui/material";
 import Tab from "@material-ui/core/Tab";
 import RevenueTable from "./tables/RevenueTable";
 import CheckUpTable from "./tables/CheckUpTable";
 import ServiceTable from "./tables/ServiceTable";
 import { axiosPrivate } from "../../api/axiosInstance";
-import { GET_ALL_SERVICES } from "../../common/constants/apiConstants";
+import {
+  COUNT_NUMBER_VISITED_BY_RANGE_TIME,
+  GET_ALL_SERVICES,
+  GET_ALL_TURN_OVER_RANGE_TIME,
+  GET_USER_WITH_SERVICE,
+} from "../../common/constants/apiConstants";
+import moment from "moment/moment";
+import { formatYearMonthDate } from "../../common/utils/formatDate";
+import dayjs from "dayjs";
+import UserServiceTable from "./tables/UserServiceTable";
+import UserCancelServiceTable from "./tables/UserCancelServiceTable";
 
 const service = [
-  {
-    maDichVu: "123456",
-    tenDichVu: "Trám Răng",
-    chiPhi: "12200000",
-  },
+  // {
+  //   maDichVu: "123456",
+  //   tenDichVu: "Trám Răng",
+  //   chiPhi: "12200000",
+  // },
 ];
 
 const checkUp = [
-  {
-    fullName: "Lại Nguyễn Tấn Tài",
-    phoneNumber: "021312334",
-    dob: "2000-02-02",
-    address: "Bình Dương",
-    dateRecord: "2000-03-03",
-  },
+  // {
+  //   fullName: "Lại Nguyễn Tấn Tài",
+  //   phoneNumber: "021312334",
+  //   dob: "2000-02-02",
+  //   address: "Bình Dương",
+  //   dateRecord: "2000-03-03",
+  // },
 ];
 
+const revenue = [
+  // {
+  //   id: "158",
+  //   date: "2022-11-11",
+  //   money: "30,150,000",
+  // },
+];
 
+const userWithService = [
+  // {
+  //   id: "158",
+  //   date: "2022-11-11",
+  //   money: "30,150,000",
+  // },
+];
 
 const OwnerTest = () => {
   const [serviceData, setServiceData] = useState(service);
-  const [checkUpData, setCheckUpeData] = useState(checkUp);
-  // const [loading, setLoading] = useState(false);
+  const [checkUpData, setCheckUpData] = useState(checkUp);
+  const [revenueData, setRevenueData] = useState(revenue);
+  const [userServiceData, setUserServiceData] = useState(userWithService);
+
+  const [isloading, setIsLoading] = useState(false);
+
   const [value, setValue] = React.useState("1");
 
+  const [textSnackbar, setTextSnackbar] = useState("");
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const current = new Date();
+  const date = `${current.getFullYear()}-${
+    current.getMonth() + 1
+  }-${current.getDate()}`;
+
+  const datePast = formatYearMonthDate(dayjs(date, "YYYY-MM-DD").add(-1, 'days'));
+
   useEffect(() => {
-    // setLoading(true);
-    fetchData();
+    loadData();
   }, []);
 
+  const loadData = async () => {
+    fetchData();
+    fetchDataCheckUp();
+    fetchDataRevenue();
+    fetchDataUserService();
+  };
+
   const fetchData = async () => {
-    // setLoading(true);
+    setIsLoading(true);
     const params = {};
-    const response = await axiosPrivate.post(GET_ALL_SERVICES, params);
-    // console.log(response);
-    const data = [...response.data];
-    setServiceData(data);
-    // setLoading(false);
+    try {
+      const response = await axiosPrivate.post(GET_ALL_SERVICES, params);
+      const data = [...response.data];
+      setServiceData(data);
+      setIsLoading(false);
+    } catch (error) {
+      setTextSnackbar("Đã xãy ra lỗi");
+      setOpenSnackbar(true);
+      setIsLoading(false);
+    }
+  };
+
+  const fetchDataCheckUp = async () => {
+    // console.log(date);
+    setIsLoading(true);
+    const params = {
+      from: "2022-11-22",
+      to: "2022-11-22",
+    };
+    // console.log(params);
+    try {
+      const response = await axiosPrivate.post(
+        COUNT_NUMBER_VISITED_BY_RANGE_TIME,
+        params
+      );
+      const data = [...response.data];
+      setCheckUpData(data);
+      setIsLoading(false);
+    } catch (error) {
+      setTextSnackbar("Đã xãy ra lỗi");
+      setOpenSnackbar(true);
+      setIsLoading(false);
+    }
+  };
+
+  const fetchDataUserService = async () => {
+    setIsLoading(true);
+    const params = {
+      date: date,
+    };
+    try {
+      const response = await axiosPrivate.post(
+        GET_USER_WITH_SERVICE,
+        params
+      );
+      const data = [...response.data];
+      setUserServiceData(data);
+      setIsLoading(false);
+    } catch (error) {
+      setTextSnackbar("Đã xãy ra lỗi");
+      setOpenSnackbar(true);
+      setIsLoading(false);
+    }
+  };
+
+  const fetchDataRevenue = async () => {
+    setIsLoading(true);
+    const params = {
+      from: datePast,
+      to: date,
+    };
+    // console.log(params);
+    try {
+      const response = await axiosPrivate.post(
+        GET_ALL_TURN_OVER_RANGE_TIME,
+        params
+      );
+      const data = [...response.data];
+      setRevenueData(data);
+      setIsLoading(false);
+    } catch (error) {
+      setTextSnackbar("Đã xãy ra lỗi");
+      setOpenSnackbar(true);
+      setIsLoading(false);
+    }
   };
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
+  };
+
   return (
-    <div>
+    <>
       <TabContext value={value}>
         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
           <TabList onChange={handleChange} aria-label="lab API tabs example">
             <Tab label="Quản Lý Dịch Vụ" value="1" />
             <Tab label="Quản Lý Lượng Người Tới Khám" value="2" />
             <Tab label="Quản Lý Doanh Thu" value="3" />
+            <Tab label="Quản Lý Lượng Người Dùng Dịch Vụ" value="4" />
+            <Tab label="Quản Lý Lượng Người Hủy Dịch Vụ" value="5" />
           </TabList>
         </Box>
         <TabPanel value="1">
-          <ServiceTable serviceData={serviceData}/>
+          <ServiceTable serviceData={serviceData} loading={isloading} />
         </TabPanel>
         <TabPanel value="2">
-          <CheckUpTable/>
+          <CheckUpTable checkUpData={checkUpData} loading={isloading} />
         </TabPanel>
         <TabPanel value="3">
-          <RevenueTable/>
+          <RevenueTable revenueData={revenueData} loading={isloading} />
+        </TabPanel>
+        <TabPanel value="4">
+          <UserServiceTable userServiceData={userServiceData} loading={isloading} />
+        </TabPanel>
+        <TabPanel value="5">
+          <UserCancelServiceTable loading={isloading} />
         </TabPanel>
       </TabContext>
-    </div>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      >
+        <Alert variant="outlined" severity="error" sx={{ width: "100%" }}>
+          {textSnackbar}
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
 
---
+++ src/pages/owner/tables/UserCancelServiceTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserCancelServiceTable = (props) => {
+  return (
+    <div>
+      <h1>Bảng Lượng Người Hủy Dịch Vụ</h1>
+    </div>
+  )
+}
+
+export default UserCancelServiceTable
